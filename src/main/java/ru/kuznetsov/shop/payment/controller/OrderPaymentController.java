0abd5330cb8d3dc2593df8623a6ce3aabdf54690
Pavel Kuznetsov <Technic47@gmail.com>
2025-12-24 16:31:23 +0300

    Updates and fixes.

--- src/main/java/ru/kuznetsov/shop/payment/controller/OrderPaymentController.java
+++ src/main/java/ru/kuznetsov/shop/payment/controller/OrderPaymentController.java
@@ -1,13 +1,13 @@
 package ru.kuznetsov.shop.payment.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.kuznetsov.shop.payment.service.PaymentsProcessingService;
+import ru.kuznetsov.shop.represent.dto.order.OrderStatusDto;
 
 @RestController
 @RequestMapping("/payment")
@@ -17,11 +17,7 @@ public class OrderPaymentController {
     private final PaymentsProcessingService paymentsProcessingService;
 
     @PostMapping("/success")
-    public ResponseEntity<Boolean> success(@RequestParam Long orderId) {
-        if (paymentsProcessingService.processPaymentSuccessStatus(orderId)) {
-            return ResponseEntity.ok(true);
-        } else {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+    public ResponseEntity<OrderStatusDto> success(@RequestParam Long orderId) {
+        return ResponseEntity.ok(paymentsProcessingService.processPaymentSuccessStatus(orderId));
     }
 }
--- src/main/java/ru/kuznetsov/shop/payment/service/PaymentsProcessingService.java
+++ src/main/java/ru/kuznetsov/shop/payment/service/PaymentsProcessingService.java
@@ -4,11 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import ru.kuznetsov.shop.kafka.service.KafkaService;
 import ru.kuznetsov.shop.represent.contract.order.OrderStatusContract;
 import ru.kuznetsov.shop.represent.dto.order.OrderStatusDto;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 
+import static ru.kuznetsov.shop.represent.common.KafkaConst.ORDER_STATUS_PAYMENT_SUCCESS_TOPIC;
 import static ru.kuznetsov.shop.represent.enums.OrderStatusType.AWAIT_PAYMENT;
 import static ru.kuznetsov.shop.represent.enums.OrderStatusType.READY;
 
@@ -16,35 +19,36 @@ import static ru.kuznetsov.shop.represent.enums.OrderStatusType.READY;
 @RequiredArgsConstructor
 public class PaymentsProcessingService {
 
+    private final KafkaService kafkaService;
     private final OrderStatusContract orderStatusService;
 
     Logger logger = LoggerFactory.getLogger(PaymentsProcessingService.class);
 
-    public Boolean processPaymentSuccessStatus(Long orderId) {
-        return saveStatus(new OrderStatusDto(
+    public OrderStatusDto processPaymentSuccessStatus(Long orderId) {
+        OrderStatusDto orderStatusSaved = orderStatusService.create(new OrderStatusDto(
                 READY,
                 "Payment service",
                 "Payment received " + LocalDateTime.now(),
                 orderId
         ));
+
+        kafkaService.sendMessageWithEntity(orderStatusSaved,
+                ORDER_STATUS_PAYMENT_SUCCESS_TOPIC,
+                Collections.emptyMap());
+
+        logger.info("Payment sent successfully received for order: {}", orderId);
+        return orderStatusSaved;
     }
 
-    public Boolean processAwaitPaymentStatus(Long orderId) {
-        return saveStatus(new OrderStatusDto(
+    public OrderStatusDto processAwaitPaymentStatus(Long orderId) {
+        OrderStatusDto paymentService = orderStatusService.create(new OrderStatusDto(
                 AWAIT_PAYMENT,
                 "Payment service",
-                "Payment received " + LocalDateTime.now(),
+                "Await payment " + LocalDateTime.now(),
                 orderId
         ));
-    }
 
-    private boolean saveStatus(OrderStatusDto orderStatusDto) {
-        try {
-            orderStatusService.create(orderStatusDto);
-            return true;
-        } catch (Exception ex) {
-            logger.warn(ex.getMessage(), ex);
-            return false;
-        }
+        logger.info("Order: {} is awaiting for payment", orderId);
+        return paymentService;
     }
 }
