3256ec4537a88c29b05142675ef4c7e09ec8ff09
Pavel Kuznetsov <Technic47@gmail.com>
2025-12-24 16:53:43 +0300

    Updated status kafka message sending removed.

--- src/main/java/ru/kuznetsov/shop/payment/PaymentModuleApplication.java
+++ src/main/java/ru/kuznetsov/shop/payment/PaymentModuleApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import ru.kuznetsov.shop.business.access.config.DataAccessConfig;
-import ru.kuznetsov.shop.kafka.config.KafkaConfig;
 
 @SpringBootApplication
-@Import({DataAccessConfig.class, KafkaConfig.class})
+@Import({DataAccessConfig.class})
 public class PaymentModuleApplication {
 
     public static void main(String[] args) {
--- src/main/java/ru/kuznetsov/shop/payment/service/PaymentsProcessingService.java
+++ src/main/java/ru/kuznetsov/shop/payment/service/PaymentsProcessingService.java
@@ -4,14 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import ru.kuznetsov.shop.kafka.service.KafkaService;
 import ru.kuznetsov.shop.represent.contract.order.OrderStatusContract;
 import ru.kuznetsov.shop.represent.dto.order.OrderStatusDto;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
 
-import static ru.kuznetsov.shop.represent.common.KafkaConst.ORDER_STATUS_PAYMENT_SUCCESS_TOPIC;
 import static ru.kuznetsov.shop.represent.enums.OrderStatusType.AWAIT_PAYMENT;
 import static ru.kuznetsov.shop.represent.enums.OrderStatusType.READY;
 
@@ -19,7 +16,6 @@ import static ru.kuznetsov.shop.represent.enums.OrderStatusType.READY;
 @RequiredArgsConstructor
 public class PaymentsProcessingService {
 
-    private final KafkaService kafkaService;
     private final OrderStatusContract orderStatusService;
 
     Logger logger = LoggerFactory.getLogger(PaymentsProcessingService.class);
@@ -31,10 +27,6 @@ public class PaymentsProcessingService {
                 "Payment received " + LocalDateTime.now(),
                 orderId
         ));
-
-        kafkaService.sendMessageWithEntity(orderStatusSaved,
-                ORDER_STATUS_PAYMENT_SUCCESS_TOPIC,
-                Collections.emptyMap());
 
         logger.info("Payment sent successfully received for order: {}", orderId);
         return orderStatusSaved;
