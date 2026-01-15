f343af0df26fe9dd3f375ad90cc02778c62cf6d4
Pavel Kuznetsov <Technic47@gmail.com>
2026-01-15 18:28:16 +0300

    Eureka discovery connection added.

--- src/main/java/ru/kuznetsov/shop/payment/PaymentModuleApplication.java
+++ src/main/java/ru/kuznetsov/shop/payment/PaymentModuleApplication.java
@@ -2,11 +2,13 @@ package ru.kuznetsov.shop.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 import ru.kuznetsov.shop.business.access.config.DataAccessConfig;
 import ru.kuznetsov.shop.kafka.config.KafkaConfig;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @Import({DataAccessConfig.class, KafkaConfig.class})
 public class PaymentModuleApplication {
 
